edf2e8798e02732c60690e0c3af3d5ab61d31bfe
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2023-01-12 16:16:39 +0900

    サーバーやクライアントからデータを取得できる関数の定義

---
+++  data.py
@@ -0,0 +1,28 @@
+import json
+
+def get_client_data():
+
+   #自分が出したカード
+    played_card = 5
+    
+    return played_card
+   
+def get_server_data():
+    server_data = {
+        #プレイヤーID
+        "player_id": 7,
+        #場に出ているカード
+        "field_card": 3,
+        #誰のターンか
+        "turn": 1,
+        #ゲームが終了しているか
+        "gameset":True,
+        #誰が買ったか
+        "winer": 1,
+        #それぞれのプレイヤーの残り枚数
+        "remaining_number_list":[],
+        #自分の持っているカード
+        "my_card_list":[]
+    } 
+
+    return json.dump(server_data)
