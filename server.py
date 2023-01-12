ffdbec01c6024eeb7f32ef6979d710ecff66af1d
Shoki-44 <k21073kk@aitech.ac.jp>
2023-01-12 10:13:05 +0900

    一人だけカードの枚数が極端に少なくなる仕様を均等に分配されるように修正

--- server.py
+++ server.py
@@ -37,14 +37,25 @@ def distribute_cards():
     random.shuffle(card_list)
     
     #カードの総枚数が人数で割り切れない場合は一人だけ枚数が少なくなる(要修正?)
-    num = math.ceil(52/player)
-    cards = [card_list[i:i + num] for i in range(0, len(card_list), num)]
-    
+    # num = math.ceil(52/player)
+    # cards = [card_list[i:i + num] for i in range(0, len(card_list), num)]
+
+    #↑の修正案
+    num_split = [(len(card_list) + i) // player for i in range(player)]
+    start = 0
+    end = 0
+    cards = []
+    for i in range(player):
+        end += num_split[i]
+        cards.append(card_list[start:end])
+        start+=num_split[i]
+        
     return cards
 
 def main():
     cards = distribute_cards()
     player_list = []
+
     for i in range(player):
         player_list += [PlayerData(i, cards[i])]
         print(player_list[i])
