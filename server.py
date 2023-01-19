33809d573d21c467bd25db1352e79ee2e305c5ab
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2023-01-19 13:44:14 +0900

    クライアントとの接続処理の追加

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ import socket
 import random
 import json
 
+PORT = 50005
+BUFFER_SIZE = 1024
+#AF_INET=アドレスファミリーを示す関数　SOCK_STREAM=ソケットタイプを示す関数
+
 player = 4
 top_card = ["T14", 0]
 revolution = False
@@ -87,6 +91,17 @@ def check_strength(top, put):
     else:
         return False
 
+def recv_client(sock, addr, id):
+    print('Client connected', addr)
+    while True:
+        #コネクションの相手側からsendを使用して送られた
+        # オブジェクトを返す何か受け取るまでブロック
+        data = sock.recv(BUFFER_SIZE).decode()
+        print(data)
+        if data == 'q':
+            break
+        sock.send(data.upper( ).encode())
+
 def main():
     global revolution, player
     cards = distribute_cards()
@@ -100,6 +115,23 @@ def main():
     random.shuffle(order)
 
     #クライアントとの接続処理
+    #４人繋がるまでここで待つ
+    #4人繋がったら下の処理を行う
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        
+        #ソケット登録   
+        s.bind(('10.1.53.93', PORT))
+        #ソケット接続待機
+        s.listen()
+
+        for i in range(4):
+            (connection, client) = s.accept()
+                # スレッドクラスのインスタンス化
+            thread = threading.Thread(target=recv_client, args=(connection, client, i))
+            # スレッド処理開始
+            thread.start()
+
 
     rank = []
     turn = 0
